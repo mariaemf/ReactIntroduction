fb2a659639121e7dfc40fbd1f81df6d65e4b5cfd
mariaemf <mariaeduardamartinsf@gmail.com>
2022-08-31 22:15:50 -0300

    atualização ao button sidebar

--- src/componentes/Sidebar.jsx
+++ src/componentes/Sidebar.jsx
@@ -1,16 +1,21 @@
 import styles from './Sidebar.module.css'
-
+import {PencilLine} from 'phosphor-react'
 export function Sidebar() {
 
   return (
     <aside className={styles.sidebar}>
       <img className={styles.cover} src="https://images.unsplash.com/photo-1660526224364-8f71a4a09514?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=500&q=80" alt="" />
     <div className={styles.profile}>
+      <img className={styles.avatar} src="https://github.com//mariaemf.png" />
+
+
       <strong>Maria Freitas</strong>
       <span>Web Developer</span>
     </div>
     <footer>
-      <a href="#">Edite seu perfil
+      <a href="#">
+        <PencilLine size={20}/>
+        Edite seu perfil
       </a>
     </footer>
     </aside>
