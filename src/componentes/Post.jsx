cab06a62bf979cbe2c00ce35ce29486140c80ddc
mariaemf <mariaeduardamartinsf@gmail.com>
2022-09-07 17:25:11 -0300

    novas estilizações de horas e post

--- src/componentes/Post.jsx
+++ src/componentes/Post.jsx
@@ -1,33 +1,45 @@
+
 import { Avatar } from './Avatar';
 import { Comment } from './comment';
 import styles from './Post.module.css';
 
 
-
-
-
-export function Post(props) {
+export function Post({author, publishedAt, content}) {
+  const publishedDateFormatted = new Intl.DateTimeFormat('PT-BR', {
+    day: '2-digit',
+    month: 'long',
+    hour: '2-digit',
+    minute: '2-digit'
+  }).format(publishedAt)
 
   return (
   <article className={styles.post}>
     <header>
       <div className={styles.author}>
         <Avatar hasBorder={true} 
-          src={props.author.avatarUrl} alt="" />
+          src={author.avatarUrl} alt="" />
         <div className={styles.authorInfor}>
-         <strong>{props.author.name}</strong>
-          <span>{props.author.role}</span>
+         <strong>{author.name}</strong>
+          <span>{author.role}</span>
         </div>  
       </div> 
-      <time title='11 de agosto ás 19:53' dateTime='2022-09-01 19:52:30-'>Publica há 1h</time>
+      <time title='07 de Setetembro às 17:17h' dateTime='2022-09-01 19:52:30-'> 
+      {publishedDateFormatted}
+      
+      </time>
     </header>
     <div className={styles.content}>
+      {content.map(line => { 
+        if (line.type === 'paragraph') {
+          return <p>{line.content}</p>
+        } else if(line.type === 'link'){
+          return <p><a href="#">{line.content}</a></p>
+        }
+      })}
+
   
    
-     <p>
-      <a href=''>#novoprojeto </a>{'  '}
-      <a href=''>#nlw </a>{'  '}
-      <a href=''>#rocketseat</a></p>
+     
     </div>
     <form className={styles.comentForm}>
       <strong>Deixe o seu Feedback </strong>
